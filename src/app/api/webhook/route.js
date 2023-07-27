a483f8c11f3cb4f1dbb9fd2f134159544a46ad41
aferraro188 <aferraro@vazkor.com>
2023-07-27 14:43:45 -0400

    update

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -3,26 +3,36 @@ import { transcript, translate } from '../../../../utilities';
 
 export async function POST(req) {
     let message
-    const targetLanguage = 'en'
     const twiml = new MessagingResponse();
-    const { searchParams } = new URL(req.url)
-    const NumMedia = searchParams.get("NumMedia")
-    const MediaUrl0 = searchParams.get("MediaUrl0")
+    try {
+        const targetLanguage = 'en'
+        const { searchParams } = new URL(req.url)
+        const NumMedia = searchParams.get("NumMedia")
+        const MediaUrl0 = searchParams.get("MediaUrl0")
 
 
-    if (NumMedia === "0") {
-        message = twiml.message("No media attached")
-        return new Response(message, {
-            status: 200,
-            headers: { 'Content-Type': 'text/xml' }
-        })
-    } else {
-        const transcriptedText = await transcript(MediaUrl0)
-        const translatedText = await translate(transcriptedText, targetLanguage)
-        message = twiml.message(transcriptedText + "\n" + translatedText).toString()
+        if (NumMedia === "0") {
+            message = twiml.message("No media attached")
+            return new Response(message, {
+                status: 200,
+                headers: { 'Content-Type': 'text/xml' }
+            })
+        } else {
+            const transcriptedText = await transcript(MediaUrl0)
+            const translatedText = await translate(transcriptedText, targetLanguage)
+            message = twiml.message(transcriptedText + "\n" + translatedText).toString()
+            return new Response(message, {
+                status: 200,
+                headers: { 'Content-Type': 'text/xml' }
+            })
+        }
+    }
+    catch (ex) {
+        message = twiml.message(`name: ${ex.name}, message: ${ex.message}, stack: ${ex.stack}`)
         return new Response(message, {
             status: 200,
             headers: { 'Content-Type': 'text/xml' }
         })
     }
+    
 }
