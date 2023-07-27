3c35112e50d353b69369523084f49249d619452a
aferraro188 <aferraro@vazkor.com>
2023-07-26 21:52:36 -0400

    update

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -2,10 +2,18 @@ import MessagingResponse from 'twilio/lib/twiml/MessagingResponse'
 import { transcript } from '../../../../utilities';
 
 export async function POST(req) {
-    const body = await req.json()
+    const { searchParams } = new URL(req.url)
+    const NumMedia = searchParams.get("NumMedia")
+    const MediaUrl0 = searchParams.get("MediaUrl0")
     const twiml = new MessagingResponse();
-    const transcriptedText = await transcript(body.MediaUrl0)
-    const message = twiml.message(transcriptedText).toString()
+    let message
+    if (NumMedia === "1") {
+        const transcriptedText = await transcript(MediaUrl0)
+        message = twiml.message(transcriptedText).toString()
+    } else {
+        message = twiml.message("Enter the correct number of media").toString()
+    }
+
     return new Response(message, {
         status: 200,
         headers: { 'Content-Type': 'text/xml' }
